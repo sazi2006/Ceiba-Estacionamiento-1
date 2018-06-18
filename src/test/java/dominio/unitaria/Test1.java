dadac3c7ba72ab777ab8feb16b4d00fcb031fc06
Valentin Quintero <vqc25000@gmail.com>
2018-06-18 13:33:25 -0500

    Se creo la estructura minima del proyecto

---
+++ src/test/java/dominio/unitaria/Test1.java
@@ -0,0 +1,23 @@
+package dominio.unitaria;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Test1 {
+
+	private static final int a = 1;
+
+	@Test
+	public void sumaTest() {
+		// arrange
+		int b;
+		// act
+		b = a + 2;
+
+		// assert
+		assertEquals(3, b);
+	}
+	
+
+}
